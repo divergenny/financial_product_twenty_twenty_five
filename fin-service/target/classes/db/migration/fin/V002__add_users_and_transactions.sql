76e6c6caec21a2ffca3cec1cde442b9268d6a999
divergenny <divergenny@gmail.com>
2025-07-16 18:24:51 +0500

    chore: remove target directories from tracking and update .gitignore

--- fin-service/target/classes/db/migration/fin/V002__add_users_and_transactions.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO employees (name, balance)
-VALUES ('Vlad', 3000),
-       ('Igor', 2000);
-
-INSERT INTO transactions (employee_id, amount, timestamp)
-VALUES (1, 5000, now() - interval '1 day'),
-       (1, -200, now()),
-       (2, 1000, now() - interval '1 day'),
-       (2, -100, now());
